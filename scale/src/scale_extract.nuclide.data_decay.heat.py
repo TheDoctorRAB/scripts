99ad7e55fb974903f94ae2b3aebedb681b986ad0
TheDoctorRAB <r.angelo.borrelli@gmail.com>
2018-02-08 18:13:31 -0700

    scale scripts

---
+++ scale/src/scale_extract.nuclide.data_decay.heat.py
@@ -0,0 +1,43 @@
+#######
+# R.A.Borrelli
+# @TheDoctorRAB
+#######
+# Extract nuclide data from scale output files
+#######
+#
+#
+####### imports
+import os
+from sys import argv
+script,scale_output=argv
+#######
+#
+####### open scale output file
+scale_file=open(scale_output,'r').readlines()
+#######
+#
+####### prepare output file
+scale_output_file=os.path.splitext(scale_output)[0]+'_decay.heat.out'
+#######
+#
+###### open file for writing
+nuclide_file=open(scale_output_file,'w+')
+######
+#
+###### initialize flag
+extract=False
+######
+#
+###### extract data
+for line in scale_file:
+    if line.strip() == "=   Nuclide concentrations in watts for case 'decay' (#2/2)                                                             =":
+        extract = True
+    elif line.strip() == "=========================================================================================================================":
+        extract = False
+    elif extract:
+        nuclide_file.write(line)
+#######
+#
+####### close file
+nuclide_file.close()
+#######
