8df0f7ac274fd8c201c0d56253160d8d46f2022a
R. A. Borrelli <r.angelo.borrelli@gmail.com>
2018-10-10 13:04:04 -0600

    container

---
+++ bash/src/igem/container.sh
@@ -0,0 +1,50 @@
+python ~/github/scripts/mcnp/src/igem/current/mcnp_calculate.container.py 12cm/single.assembly_7815.inp
+python ~/github/scripts/mcnp/src/igem/current/mcnp_calculate.container.py 1.5cm/single.assembly_7815.inp
+python ~/github/scripts/mcnp/src/igem/current/mcnp_calculate.container.py 18cm/single.assembly_7815.inp
+python ~/github/scripts/mcnp/src/igem/current/mcnp_calculate.container.py 30cm/single.assembly_7815.inp
+python ~/github/scripts/mcnp/src/igem/current/mcnp_calculate.container.py 3cm/single.assembly_7815.inp
+python ~/github/scripts/mcnp/src/igem/current/mcnp_calculate.container.py 60cm/single.assembly_7815.inp
+python ~/github/scripts/mcnp/src/igem/current/mcnp_calculate.container.py 6cm/single.assembly_7815.inp
+
+mv igem_surface.card_single.assembly_3.0cm_1.5cm.out 1.5cm/
+mv igem_surface.card_single.assembly_5.0cm_1.5cm.out 1.5cm/
+mv igem_surface.card_single.assembly_10.0cm_1.5cm.out 1.5cm/
+mv igem_surface.card_single.assembly_15.0cm_1.5cm.out 1.5cm/
+mv igem_surface.card_single.assembly_20.0cm_1.5cm.out 1.5cm/
+
+mv igem_surface.card_single.assembly_3.0cm_3.0cm.out 3cm/
+mv igem_surface.card_single.assembly_5.0cm_3.0cm.out 3cm/
+mv igem_surface.card_single.assembly_10.0cm_3.0cm.out 3cm/
+mv igem_surface.card_single.assembly_15.0cm_3.0cm.out 3cm/
+mv igem_surface.card_single.assembly_20.0cm_3.0cm.out 3cm/
+
+mv igem_surface.card_single.assembly_3.0cm_6.0cm.out 6cm/
+mv igem_surface.card_single.assembly_5.0cm_6.0cm.out 6cm/
+mv igem_surface.card_single.assembly_10.0cm_6.0cm.out 6cm/
+mv igem_surface.card_single.assembly_15.0cm_6.0cm.out 6cm/
+mv igem_surface.card_single.assembly_20.0cm_6.0cm.out 6cm/
+
+mv igem_surface.card_single.assembly_3.0cm_12.0cm.out 12cm/
+mv igem_surface.card_single.assembly_5.0cm_12.0cm.out 12cm/
+mv igem_surface.card_single.assembly_10.0cm_12.0cm.out 12cm/
+mv igem_surface.card_single.assembly_15.0cm_12.0cm.out 12cm/
+mv igem_surface.card_single.assembly_20.0cm_12.0cm.out 12cm/
+
+mv igem_surface.card_single.assembly_3.0cm_18.0cm.out 18cm/
+mv igem_surface.card_single.assembly_5.0cm_18.0cm.out 18cm/
+mv igem_surface.card_single.assembly_10.0cm_18.0cm.out 18cm/
+mv igem_surface.card_single.assembly_15.0cm_18.0cm.out 18cm/
+mv igem_surface.card_single.assembly_20.0cm_18.0cm.out 18cm/
+
+mv igem_surface.card_single.assembly_3.0cm_30.0cm.out 30cm/
+mv igem_surface.card_single.assembly_5.0cm_30.0cm.out 30cm/
+mv igem_surface.card_single.assembly_10.0cm_30.0cm.out 30cm/
+mv igem_surface.card_single.assembly_15.0cm_30.0cm.out 30cm/
+mv igem_surface.card_single.assembly_20.0cm_30.0cm.out 30cm/
+
+mv igem_surface.card_single.assembly_3.0cm_60.0cm.out 60cm/
+mv igem_surface.card_single.assembly_5.0cm_60.0cm.out 60cm/
+mv igem_surface.card_single.assembly_10.0cm_60.0cm.out 60cm/
+mv igem_surface.card_single.assembly_15.0cm_60.0cm.out 60cm/
+mv igem_surface.card_single.assembly_20.0cm_60.0cm.out 60cm/
+
