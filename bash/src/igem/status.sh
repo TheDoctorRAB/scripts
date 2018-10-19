4c45dce3e61f90a9d40476fd32e1bde99af8edd1
R. A. Borrelli <r.angelo.borrelli@gmail.com>
2018-10-19 15:46:55 -0600

    status

---
+++ bash/src/igem/status.sh
@@ -0,0 +1,63 @@
+cd 10wt/
+cd 1.5cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../3cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../6cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../12cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../18cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../30cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../../30wt/1.5cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../3cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../6cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../12cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../18cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../30cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../../50wt/1.5cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../3cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../6cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../12cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../18cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../30cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../../70wt/1.5cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../3cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../6cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../12cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../18cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../30cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../../90wt/1.5cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../3cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../6cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../12cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../18cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../30cm/out
+python ~/github/scripts/mcnp/src/output/mcnp_status.check.py single.assembly_7815.inp.o
+cd ../../../../
+
