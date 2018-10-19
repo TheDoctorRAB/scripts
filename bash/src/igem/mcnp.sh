d6397d795f7515b8701ae6a82c135e2afdd85a14
R. A. Borrelli <r.angelo.borrelli@gmail.com>
2018-10-19 16:32:22 -0600

    mcnp

--- bash/src/igem/mcnp.sh
+++ bash/src/igem/mcnp.sh
@@ -1,63 +1,63 @@
 cd 10wt/
 cd 1.5cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../3cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../6cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../12cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../18cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../30cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../../30wt/1.5cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../3cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../6cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../12cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../18cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../30cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../../50wt/1.5cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../3cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../6cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../12cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../18cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../30cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../../70wt/1.5cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../3cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../6cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../12cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../18cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../30cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../../90wt/1.5cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../3cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../6cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../12cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../18cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../30cm/inp
-sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:45:00 -n16 -P iuc
+sub_mcnpx_2.7.0 -i single.assembly_7815.inp -w 00:50:00 -n16 -P iuc
 cd ../../../../
 
