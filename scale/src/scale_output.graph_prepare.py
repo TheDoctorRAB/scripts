6c8b62caf27416abe4ed3179a339fb9944926a0a
R. A. Borrelli <r.angelo.borrelli@gmail.com>
2019-04-22 15:11:56 -0600

    graphing preparation file complete

--- scale/src/scale_output.graph_prepare.py
+++ scale/src/scale_output.graph_prepare.py
@@ -41,6 +41,45 @@ time_data=numpy.genfromtxt(timefile,dtype=float)
 #
 rows=labels.shape[0]
 columns=raw_data.shape[1]
-print rows,columns
+#print rows,columns
 #
+#######
+#
+# raw data is given per MTU
+# compute per assembly
+#
+mtu_per_assembly=0.475
+#
+assembly_calculation=numpy.zeros(columns)
+#
+for j in range(0,columns):
+    assembly_calculation[j]=mtu_per_assembly*raw_data[rows-1,j]
+#
+#######
+#
+# prepare output file
+#
+output_file=os.path.splitext(datafile)[0]+'_'+labels[rows-1]+'_to.graph.out'
+#
+#######
+#
+# open file for writing
+#
+graph_preparation=open(output_file,'w+')
+#
+#######
+#
+# write to file
+#
+for j in range(0,columns):
+    graph_preparation.write(str.format('%.2f'%time_data[j])+'\t'+str.format('%.4e'%raw_data[rows-1,j])+'\t'+str.format('%.4e'%assembly_calculation[j])+'\n')
+#
+#######
+#
+# close file
+#
+graph_preparation.close()
+#
+#######
+# EOF
 #######
