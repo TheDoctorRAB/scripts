48248a78a93e6a93d06935dc51f54516b490878e
R. A. Borrelli <r.angelo.borrelli@gmail.com>
2019-04-23 16:01:18 -0600

    update

--- mcnp/src/material-card/mcnp_material.card_weight.fraction.py
+++ mcnp/src/material-card/mcnp_material.card_weight.fraction.py
@@ -4,22 +4,23 @@
 #
 # prepare material card for an mcnp input file
 # assumes a file with radionuclide name in column 1
-# remaining columns are mass or mass fraction
+# remaining columns are mass fraction*1E6 based on origen output
+# for use with _fixed.out file
+# remember file has 'totals' in last row
 #
 #######
 #
-# currently built for origen output
-# computes weight fraction
+# computes weight fraction in mcnp style
 #
 #######
 #
 # imports
 import os
 import numpy
-import pyne
+#import pyne
 from sys import argv
-from pyne import nucname
-script,timefile,datafile=argv
+#from pyne import nucname - installation problems with pyne
+script,datafile,timefile=argv
 #
 #######
 #
@@ -28,6 +29,13 @@ script,timefile,datafile=argv
 labels=numpy.genfromtxt(datafile,dtype=object,usecols=[0])
 raw_data=numpy.genfromtxt(datafile,dtype=float)[:,1:]
 time=numpy.genfromtxt(timefile,dtype=str) #read in as string because time step is used as output file label
+#
+#######
+#
+# weight fraction cutoff
+# after calculating actual weight fraction
+#
+cutoff=1E-06
 #
 #######
 #
@@ -40,9 +48,9 @@ columns=raw_data.shape[1]
 #
 # compute weight fraction
 #
-weight_fraction=numpy.zeros((rows,columns))
+weight_fraction=numpy.zeros((rows-2,columns))
 #
-for i in range (0,rows):
+for i in range (0,rows-2):
   for j in range (0,columns):
      weight_fraction[i,j]=raw_data[i,j]*(-1)*1E-06 #negative is added here because MCNP uses (-) for weight fraction
 #
@@ -50,23 +58,16 @@ for i in range (0,rows):
 #
 # change to ZAID
 #
-for i in range (0,rows):
-  labels[i]=str(pyne.nucname.zzzaaa(labels[i]))+'.70c'
+#for i in range (0,rows):
+#  labels[i]=str(pyne.nucname.zzzaaa(labels[i]))+'.70c'
+#
+#######
 #
 # write weight fractions above prescribed cutoff to new file
 #
-cutoff=1E-06
-#
 for j in range(0,columns):
-###
-#
-# open material card
-#
-  material_card=open(os.path.splitext(datafile)[0]+'_material.card_'+time[j]+'.out','w+')
-#
-###
-#
-  for i in range(0,rows):
+  material_card=open(os.path.splitext(datafile)[0]+'_material.card_'+time[j]+'.out','w+') #open material card
+  for i in range(0,(rows-2)):
    if(abs(weight_fraction[i,j])>cutoff):
     material_card.write('        '+str.format('%s'%labels[i])+'     '+str.format('%.6e'%weight_fraction[i,j])+'\n')
 #
