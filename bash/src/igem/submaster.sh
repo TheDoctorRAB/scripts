525689d3a0ab5f0185cdd1d301f5083b7f1dced8
R. A. Borrelli <r.angelo.borrelli@gmail.com>
2019-05-07 12:02:09 -0600

    updates on scripts

---
+++ bash/src/igem/submaster.sh
@@ -0,0 +1,15 @@
+cd 1.5cm/
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../3cm/
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../6cm/
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../12cm/
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../18cm/
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../30cm/
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../60cm/
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../
