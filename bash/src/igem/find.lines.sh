60a1ed473eccff0de3170d4f728c222f483f0cc3
R. A. Borrelli <borrelli@localhost.localdomain>
2019-05-09 14:28:32 -0600

    find lines script

---
+++ bash/src/igem/find.lines.sh
@@ -0,0 +1,71 @@
+cd 10wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../../
+cd 30wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../../
+cd 50wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../../
+cd 70wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../../
+cd 90wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.lines.py single.assembly_7815.inp
+cd ../../../../
+
--- general/src/general_find.lines.py
+++ general/src/general_find.lines.py
@@ -15,9 +15,9 @@ script,input_file=argv
 #
 # set lines to find
 #
-find_lines1=''
+find_lines1='SDEF   POS d1 ERG d2              $SP1 has to have the same number of entries as sources'
 #
-find_lines2=''
+find_lines2='SP1    1 1444R'
 #
 #######
 #
@@ -30,11 +30,11 @@ tempfile=open(input_file,'r').readlines()
 #
 # find line numbers
 #
-for line_number1,line in enumerate(tempfile,1):
+for line_number1,line in enumerate(tempfile,1): # use 1 to give vim line number
     if find_lines1 in line:
         print find_lines1,'   ',line_number1
 ###
-for line_number2,line in enumerate(tempfile,1):
+for line_number2,line in enumerate(tempfile,1): # use 1 to give vim line number
     if find_lines2 in line:
         print find_lines2,'   ',line_number2
 #
