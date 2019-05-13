9f4f34f91ac652787d65fa81cb547a1373a3372d
R. A. Borrelli <borrelli@localhost.localdomain>
2019-05-13 11:48:56 -0600

    full find and replace

---
+++ bash/src/igem/find.replace.full.sh
@@ -0,0 +1,422 @@
+cd 0wt/
+cd 10wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 30wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 50wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 70wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 90wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../../
+cd 10wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 30wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 50wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 70wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 90wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../../
+cd 25wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 30wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 50wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 70wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 90wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../../
+cd 50wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 30wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 50wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 70wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 90wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../../
+cd 5wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 30wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 50wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 70wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 90wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../../
+cd 75wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 30wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 50wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 70wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../
+cd 90wt/
+cd 1.5cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../3cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../6cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../12cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../18cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../30cm/inp
+python ~/github/scripts/general/src/general_find.replace.py single.assembly_7815.inp
+cd ../../../../
+
