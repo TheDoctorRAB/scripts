96b28d1756e0eb5ca36c60946628efba4a62a335
R. A. Borrelli <borrelli@localhost.localdomain>
2019-05-14 13:29:48 -0600

    delete lines

--- bash/src/igem/delete.lines.sh
+++ bash/src/igem/delete.lines.sh
@@ -1,71 +1,634 @@
-cd 10wt/
-cd 1.5cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../3cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../6cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../12cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../18cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../30cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../../
-cd 30wt/
-cd 1.5cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../3cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../6cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../12cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../18cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../30cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../../
-cd 50wt/
-cd 1.5cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../3cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../6cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../12cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../18cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../30cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../../
-cd 70wt/
-cd 1.5cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../3cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../6cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../12cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../18cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../30cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../../
-cd 90wt/
-cd 1.5cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../3cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../6cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../12cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../18cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../30cm/inp
-sed -i '575,1152d' single.assembly_7815.inp
-cd ../../../../
-
+#!/usr/bin/bash
+pushd 0wt/
+echo
+echo $PWD
+echo
+pushd 10wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 30wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 50wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 70wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 90wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+popd
+pushd 10wt/
+echo
+echo $PWD
+echo
+pushd 10wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 30wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 50wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 70wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 90wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+popd
+pushd 25wt/
+echo
+echo $PWD
+echo
+pushd 10wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 30wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 50wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 70wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 90wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+popd
+pushd 50wt/
+echo
+echo $PWD
+echo
+pushd 10wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 30wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 50wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 70wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 90wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+popd
+pushd 5wt/
+echo
+echo $PWD
+echo
+pushd 10wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 30wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 50wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 70wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 90wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+popd
+pushd 75wt/
+echo
+echo $PWD
+echo
+pushd 10wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 30wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 50wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 70wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+pushd 90wt/
+pushd 1.5cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 3cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 6cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 12cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 18cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+pushd 30cm/inp/
+sed -i '554,1131d' single.assembly_7815.inp
+popd
+popd
+popd
+echo
+echo $PWD
+echo
