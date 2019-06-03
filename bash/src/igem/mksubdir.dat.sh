5d63b21eac6b07f4a9e13c614185568a778fccba
R. A. Borrelli <borrelli@localhost.localdomain>
2019-06-03 13:42:23 -0600

    subdir

---
+++ bash/src/igem/mksubdir.dat.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+mkdir -p 0wt/dat/{raw,dose-rate}
+mkdir -p 10wt/dat/{raw,dose-rate}
+mkdir -p 25wt/dat/{raw,dose-rate}
+mkdir -p 50wt/dat/{raw,dose-rate}
+mkdir -p 5wt/dat/{raw,dose-rate}
+mkdir -p 75wt/dat/{raw,dose-rate}
