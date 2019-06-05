a28380bac71e17bf230c4ff4298ed2625c7635d2
R. A. Borrelli <borrelli@localhost.localdomain>
2019-06-05 16:10:28 -0600

    graph scripts

---
+++ bash/src/igem/find.replace.graph.high.bare.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/bash
+pushd 0wt/
+echo
+echo $PWD
+echo
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_west.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_east.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_south.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_north.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_cap.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_plug.py
+popd
+pushd 10wt/
+echo
+echo $PWD
+echo
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_west.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_east.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_south.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_north.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_cap.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_plug.py
+popd
+pushd 25wt/
+echo
+echo $PWD
+echo
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_west.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_east.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_south.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_north.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_cap.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_plug.py
+popd
+pushd 50wt/
+echo
+echo $PWD
+echo
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_west.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_east.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_south.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_north.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_cap.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_plug.py
+popd
+pushd 5wt/
+echo
+echo $PWD
+echo
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_west.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_east.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_south.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_north.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_cap.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_plug.py
+popd
+pushd 75wt/
+echo
+echo $PWD
+echo
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_west.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_east.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_south.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_north.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_cap.py
+python ~/github/scripts/general/src/general_find.replace.py plot_all.in.one_cask.thickness_dose.rate_h5060_plug.py
+popd
