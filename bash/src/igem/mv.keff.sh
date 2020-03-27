f6500e824f68b4bae290b36116f0bacdb375d26e
TheDoctorRAB <TheDoctorRAB@localhost.localdomain>
2020-03-27 15:04:10 -0600

    move keff

---
+++ bash/src/igem/mv.keff.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+mv 3cm/0wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/10wt/1.5cm/out/
+mv 3cm/0wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/10wt/30cm/out/
+mv 3cm/0wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/30wt/1.5cm/out/
+mv 3cm/0wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/30wt/30cm/out/
+mv 3cm/75wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/10wt/1.5cm/out/
+mv 3cm/75wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/10wt/30cm/out/
+mv 3cm/75wt/30wt/1.5cm/single.assembly_7815.keff.inp.o 3cm/75wt/30wt/1.5cm/out/
+mv 3cm/75wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/30wt/30cm/out/
+mv 20cm/0wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 20cm/0wt/10wt/1.5cm/out/
+mv 20cm/0wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 20cm/0wt/10wt/30cm/out/
+mv 20cm/0wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 20cm/0wt/30wt/1.5cm/out/
+mv 20cm/0wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 20cm/0wt/30wt/30cm/out/
+mv 20cm/75wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 20cm/75wt/10wt/1.5cm/out/
+mv 20cm/75wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 20cm/75wt/10wt/30cm/out/
+mv 20cm/75wt/30wt/1.5cm/single.assembly_7815.keff.inp.o 20cm/75wt/30wt/1.5cm/out/
+mv 20cm/75wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 20cm/75wt/30wt/30cm/out/
