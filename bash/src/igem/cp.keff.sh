a915add5572c565abaacd681bbef4711e637e418
TheDoctorRAB <TheDoctorRAB@localhost.localdomain>
2020-03-26 18:05:09 -0600

    keff

---
+++ bash/src/igem/cp.keff.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+cp 3cm/0wt/10wt/1.5cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 3cm/0wt/10wt/30cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 3cm/0wt/30wt/1.5cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 3cm/0wt/30wt/30cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 3cm/75wt/10wt/1.5cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 3cm/75wt/10wt/30cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 3cm/75wt/30wt/1.5cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 3cm/75wt/30wt/30cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 20cm/0wt/10wt/1.5cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 20cm/0wt/10wt/30cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 20cm/0wt/30wt/1.5cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 20cm/0wt/30wt/30cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 20cm/75wt/10wt/1.5cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 20cm/75wt/10wt/30cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 20cm/75wt/30wt/1.5cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
+cp 20cm/75wt/30wt/30cm/inp/single.assembly_7815.inp single.assembly_7815.keff.inp
