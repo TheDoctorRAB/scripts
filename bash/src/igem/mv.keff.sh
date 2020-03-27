cf98fcd3f5cd6f6edb9ad58b4e6fb2a8f916fe27
TheDoctorRAB <TheDoctorRAB@localhost.localdomain>
2020-03-27 15:11:57 -0600

    grow the fuck up

--- bash/src/igem/mv.keff.sh
+++ bash/src/igem/mv.keff.sh
@@ -5,7 +5,7 @@ mv 3cm/0wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/30wt/1.5cm/out
 mv 3cm/0wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/30wt/30cm/out/
 mv 3cm/75wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/10wt/1.5cm/out/
 mv 3cm/75wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/10wt/30cm/out/
-mv 3cm/75wt/30wt/1.5cm/single.assembly_7815.keff.inp.o 3cm/75wt/30wt/1.5cm/out/
+mv 3cm/75wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/30wt/1.5cm/out/
 mv 3cm/75wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/30wt/30cm/out/
 mv 20cm/0wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 20cm/0wt/10wt/1.5cm/out/
 mv 20cm/0wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 20cm/0wt/10wt/30cm/out/
@@ -13,5 +13,5 @@ mv 20cm/0wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 20cm/0wt/30wt/1.5cm/o
 mv 20cm/0wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 20cm/0wt/30wt/30cm/out/
 mv 20cm/75wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 20cm/75wt/10wt/1.5cm/out/
 mv 20cm/75wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 20cm/75wt/10wt/30cm/out/
-mv 20cm/75wt/30wt/1.5cm/single.assembly_7815.keff.inp.o 20cm/75wt/30wt/1.5cm/out/
+mv 20cm/75wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 20cm/75wt/30wt/1.5cm/out/
 mv 20cm/75wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 20cm/75wt/30wt/30cm/out/
