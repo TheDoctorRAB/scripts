cd193c481c6e5c33b08232d6c98ff9385304cbe6
TheDoctorRAB <TheDoctorRAB@localhost.localdomain>
2020-03-27 18:26:32 -0600

    sort bare out

--- bash/src/igem/mv.keff.sh
+++ bash/src/igem/mv.keff.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 mv 0wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 0wt/10wt/1.5cm/out/
 mv 0wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 0wt/10wt/30cm/out/
 mv 0wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 0wt/30wt/1.5cm/out/
