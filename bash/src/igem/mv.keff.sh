cb3c7f6f392ca6e364d4c9a751e97b9547ee8730
TheDoctorRAB <TheDoctorRAB@localhost.localdomain>
2020-03-27 18:09:54 -0600

    sort bare out

--- bash/src/igem/mv.keff.sh
+++ bash/src/igem/mv.keff.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/bash
-mv 3cm/0wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/10wt/1.5cm/out/
-mv 3cm/0wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/10wt/30cm/out/
-mv 3cm/0wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/30wt/1.5cm/out/
-mv 3cm/0wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/30wt/30cm/out/
-mv 3cm/75wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/10wt/1.5cm/out/
-mv 3cm/75wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/10wt/30cm/out/
-mv 3cm/75wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/30wt/1.5cm/out/
-mv 3cm/75wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/30wt/30cm/out/
-mv 20cm/0wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 20cm/0wt/10wt/1.5cm/out/
-mv 20cm/0wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 20cm/0wt/10wt/30cm/out/
-mv 20cm/0wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 20cm/0wt/30wt/1.5cm/out/
-mv 20cm/0wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 20cm/0wt/30wt/30cm/out/
-mv 20cm/75wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 20cm/75wt/10wt/1.5cm/out/
-mv 20cm/75wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 20cm/75wt/10wt/30cm/out/
-mv 20cm/75wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 20cm/75wt/30wt/1.5cm/out/
-mv 20cm/75wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 20cm/75wt/30wt/30cm/out/
+mv 0wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/10wt/1.5cm/out/
+mv 0wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/10wt/30cm/out/
+mv 0wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/30wt/1.5cm/out/
+mv 0wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/0wt/30wt/30cm/out/
+mv 75wt/10wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/10wt/1.5cm/out/
+mv 75wt/10wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/10wt/30cm/out/
+mv 75wt/30wt/1.5cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/30wt/1.5cm/out/
+mv 75wt/30wt/30cm/inp/single.assembly_7815.keff.inp.o 3cm/75wt/30wt/30cm/out/
