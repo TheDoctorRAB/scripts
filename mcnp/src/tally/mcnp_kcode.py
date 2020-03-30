f355f019e6de05f0194f2b17d8912dee09c54742
TheDoctorRAB <TheDoctorRAB@localhost.localdomain>
2020-03-30 10:23:17 -0600

    keef check

---
+++ mcnp/src/tally/mcnp_kcode.py
@@ -0,0 +1,39 @@
+########################################################################
+# @TheDoctorRAB
+########################################################################
+# This finds and writes/prints the final estimated keff and error.
+########################################################################
+#
+#
+####### imports
+import os
+from sys import argv
+script,mcnp_output=argv
+#######
+#
+####### open mcnp output file
+mcnp_file=open(mcnp_output,'r').readlines()
+#######
+#
+#######
+# get and print current directory
+dir_path=os.getcwd()
+print dir_path
+#######
+#
+####### initialize the counter
+i=0
+#######
+#
+####### search the file for thekeff estimate
+# line will write the current line
+# mcnp_file[i] writes the next line (if needed)
+#
+for line in mcnp_file:
+  i=i+1
+  if '  | the final estimated' in line:
+    print line
+#
+#######
+#EOF
+#######
